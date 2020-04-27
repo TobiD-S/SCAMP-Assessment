d490681f4df3f59122a99a778462c09370fa51b7
Tobi <delesafatobi@gmail.com>
2020-04-27 22:23:43 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Creating a Fibonacci Series of a given number
-// I didn't really understand the wording of the question, and so I decided to write the two interpretations of the question that I thought of.
+// I didn't really understand the wording of the question, so I decided to write the two interpretations of the question that I thought of.
 
-// A function Fibonacci to return the nth number of the sequence
+// A function fibonacciNum to return the nth number of the sequence
 let fibonacciNum = (n) => {
     let x = 0,
         y = 1,
@@ -17,7 +17,7 @@ let fibonacciNum = (n) => {
 
 console.log(fibonacciNum(5));
 
-// A function fibonacci to return a sequence with 'm' numbers in the Fibonacci sequence
+// A function fibonacciSeq to return a sequence with 'm' numbers in the Fibonacci sequence
 let fibonacciSeq = (m) => {
     if (m === 1) {
         return [0, 1];
